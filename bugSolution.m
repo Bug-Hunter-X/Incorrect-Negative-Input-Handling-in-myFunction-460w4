ec4b0967b8c5a29c5eba4b07768a62880609e328
BugHunter-X <askzenai@gmail.com>
2025-01-21 20:04:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,11 @@
+```matlab
+function result = myFunction(input)
+  % Some code here...
+  if input < 0
+    result = -input; %Corrected line
+  else
+    result = input * 2;
+  end
+  % More code...
+end
+```
